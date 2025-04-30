0b7637347e699d773f03ee171033dc275d3a2362
SaurabhJha77 <saurabhjha77777@gmail.com>
2025-04-30 14:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "26003553"
-api_hash = "c1f27c622acecf9bf6e71d0d295e75f9"
-bot_token = "8105702063:AAGu5JqZlC2sMDoEEOnTO-FdlllRXs16-38"
-auth_users = [7924379211]
-sudo_users = [7924379211]
+api_id = "27777236"
+api_hash = "a87c4cc7e7f8dc993e97302112b6c82c"
+bot_token = "7443907070:AAHmV1OmgASGFeDRMiXXtbzzLhJI1khoFa0"
+auth_users = [8152589217,6073936818]
+sudo_users = [8152589217,6073936818]
